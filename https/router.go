eb3dd758160f57236961985cb1be231460c80f9e
Tony Chouteau <34800257+TonyChouteau@users.noreply.github.com>
2019-10-02 16:02:05 +0200

    zerht

--- https/router.go
+++ https/router.go
@@ -10,34 +10,6 @@ import (
 	"github.com/TonyChouteau/elrict3/ai"
 )
 
-/*
-Data : Struct
-*/
-/*type Data struct {
-	Board     engine.Matrix3x3 `json:"board"`
-	ListLegal []int            `json:"listLegal"`
-	IsLegal   bool             `json:"isLegal"`
-}*/
-
-/*func test(c *gin.Context) {
-	//countByType := storage.CountProjects(c)
-	board := engine.CreateM()
-	fmt.Println(board)
-
-	board, _ = engine.Play(board, 5, CROSS)
-
-	list := engine.ListLegal(board)
-	fmt.Println(list)
-	test1 := engine.IsLegal(board, 5)
-	fmt.Println(test1)
-
-	c.JSON(200, Data{
-		board,
-		list,
-		test1,
-	})
-}*/
-
 func moveAI(c *gin.Context) {
 	board := c.Param("board")
 	result := ai.GetBestMove(board)
@@ -53,7 +25,12 @@ func Serve() {
 
 	r.Use(cors.New(cors.Config{
 		AllowOrigins: []string{"https://www.tonychouteau.fr", "https://www.thomaslepercq.fr/elrict3"},
+		Methods: “GET, PUT, POST, DELETE”,
+		ExposedHeaders: “”,
+		Credentials: false,
+		ValidateHeaders: false,
 	}))
+    
 
 	r.GET("/ai/:board", moveAI)
 
